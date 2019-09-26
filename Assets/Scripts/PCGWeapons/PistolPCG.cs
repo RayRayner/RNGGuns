e7f7d5d49515489da30e0adeeff057c6c68b645a
Rayner <robrayner96@hotmail.com>
2019-09-26 17:36:58 +0100

    Added actual files

---
+++ Assets/Scripts/PCGWeapons/PistolPCG.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PistolPCG : MonoBehaviour
+{
+    public GameObject basePrefab;
+
+    public GameObject[] barrel, body, grip, sight;
+    public bool barrelDone, bodyDone, gripDone, sightDone;
+
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            GameObject pistol = (GameObject)Instantiate(basePrefab);
+            pistol.AddComponent<PistolDamage>();
+            if (!bodyDone)
+            {
+                Transform bodyPos = pistol.transform.Find("Body").gameObject.transform;
+                Instantiate(body[Random.Range(0, body.Length - 1)], bodyPos);
+                //bodyDone = true;
+            }
+            if (!barrelDone)
+            {
+                Transform barrelPos = pistol.transform.Find("Barrel").gameObject.transform;
+                Instantiate(barrel[Random.Range(0, barrel.Length - 1)], barrelPos);
+                //barrelDone = true;
+            }
+            if (!sightDone)
+            {
+                Transform sightPos = pistol.transform.Find("Sight").gameObject.transform;
+                Instantiate(sight[Random.Range(0, sight.Length - 1)], sightPos);
+                //sightDone = true;
+            }
+            if (!gripDone)
+            {
+                Transform gripPos = pistol.transform.Find("Grip").gameObject.transform;
+                Instantiate(grip[Random.Range(0, barrel.Length - 1)], gripPos);
+                //gripDone = true;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+
+            Destroy(GameObject.Find("Pistol(Clone)"));
+        }
+
+    }
+}
